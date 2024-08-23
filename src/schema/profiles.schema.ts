ff020c30edb4985719d56c22829c87b8533e26dc
zexoverz <faisalfirdani01@gmail.com>
2024-08-23 20:55:16 +0900

    Profile New Schema

--- src/schema/profiles.schema.ts
+++ src/schema/profiles.schema.ts
@@ -1,6 +1,29 @@
 import { z } from 'zod';
 
-const ProfileIPFSSchema = z.object({
+const ReviewSchema = z.object({
+    star: z.number(),
+    description: z.string(),
+    username: z.string(),
+    createdAt: z.number(),
+  });
+  
+  const EducationSchema = z.object({
+    startDate: z.number(),
+    endDate: z.number(),
+    degreeName: z.string(),
+    schoolName: z.string(),
+    description: z.string(),
+  });
+  
+  const WorkExperienceSchema = z.object({
+    startDate: z.number(),
+    endDate: z.number(),
+    roleName: z.string(),
+    companyName: z.string(),
+    description: z.string(),
+  });
+  
+  const ProfileIPFSSchema = z.object({
     username: z.string(),
     email: z.string(),
     description: z.string(),
@@ -9,8 +32,10 @@ const ProfileIPFSSchema = z.object({
     reputation: z.array(z.any()).optional(),
     pictureIPFS: z.string().optional(),
     skills: z.array(z.string()).optional(),
-    preference: z.string().optional(),
-    review: z.array(z.string()).optional(),
+    education: z.array(EducationSchema).optional(),
+    workExperience: z.array(WorkExperienceSchema).optional(),
+    review: z.array(ReviewSchema).optional(),
+    preference: z.string().optional()
   });
 
 type ProfileIPFSType = z.infer<typeof ProfileIPFSSchema>;
