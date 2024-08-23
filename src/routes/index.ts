244165e048ed6363990a067ebe00de041dbd9437
zexoverz <faisalfirdani01@gmail.com>
2024-08-23 21:02:18 +0900

    Profiles route implementation

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,9 +2,11 @@ import { Hono } from 'hono'
 import { logger } from 'hono/logger'
 
 import jobs from './jobs.route'
+import profiles from './profiles.route'
 
 export const routes = (app: Hono) => {
     app.use('*', logger())
-    
+
     app.route('/jobs', jobs)
+    app.route('/profiles', profiles)
 }
