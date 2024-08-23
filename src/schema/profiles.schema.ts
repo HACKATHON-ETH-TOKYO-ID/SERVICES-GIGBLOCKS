935da3516daa67700e437069568734c5cacf6bff
zexoverz <faisalfirdani01@gmail.com>
2024-08-23 19:49:41 +0900

    Profiles Schema new

---
+++ src/schema/profiles.schema.ts
@@ -0,0 +1,23 @@
+import { z } from 'zod';
+
+  
+  const ProfileIPFSSchema = z.object({
+    username: z.string(),
+    email: z.string(),
+    description: z.string(),
+    profileType: z.string(),
+    country: z.string().optional(),
+    reputation: z.array(z.any()).optional(),
+    pictureIPFS: z.string().optional(),
+    skills: z.array(z.string()).optional(),
+    preference: z.string().optional(),
+    review: z.array(z.string()).optional(),
+  });
+
+type ProfileIPFSType = z.infer<typeof ProfileIPFSSchema>;
+
+
+  
+
+
+export { ProfileIPFSSchema, ProfileIPFSType };
