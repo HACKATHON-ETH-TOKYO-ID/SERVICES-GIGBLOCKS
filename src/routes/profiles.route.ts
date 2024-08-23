a53439b38934f64d3fb2e7175d6cc84992371adb
zexoverz <faisalfirdani01@gmail.com>
2024-08-23 19:40:39 +0900

    upload ipfs initiallize

--- src/routes/profiles.route.ts
+++ src/routes/profiles.route.ts
@@ -7,5 +7,10 @@ profiles.get('/:wallet', async (c) => {
     return c.json({message: "Return Profile Detail"})
 })
 
+profiles.post('/uploadIpfs', async (c) => {
+    
+    return c.json({message: "PINATA RESPONSE"})
+})
+
 
 export default profiles
